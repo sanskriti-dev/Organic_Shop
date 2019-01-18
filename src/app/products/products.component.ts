e87b3b79b2fa332f012258fea08cf19a1113a0a0
Sanskriti Saluja <sanskriti.16bcs2076@abes.ac.in>
2019-01-18 18:34:05 +0530

    Added Category Table

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ProductService} from '../product.service';
 import {Product} from '../models/product';
 import {Observable} from 'rxjs';
+import {CategoryService} from '../category.service';
 
 @Component({
   selector: 'app-products',
@@ -11,14 +12,17 @@ import {Observable} from 'rxjs';
 export class ProductsComponent {
   products$ = Observable;
   key$: Observable<Product>;
+  category$ ;
 
-  constructor(private productService: ProductService) {
+  constructor(private productService: ProductService, private category: CategoryService) {
     this.products$ = this.productService.getAll();
     this.key$ = this.products$.map(changes => {
       return changes.map(c => ({
         key: c.payload.key, ...c.payload.val()
       }));
-    });
+    }
+    );
+    this.category$ = this.category.getCategory();
   }
 
 
